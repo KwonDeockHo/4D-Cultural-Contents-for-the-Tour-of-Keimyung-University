68c8c9951a9f85ace8a72a7d8a893e4616d1e3a6
KwonDeockHo <42824582+KwonDeockHo@users.noreply.github.com>
2021-10-13 21:52:47 +0900

    Update MenuSelectorMover.cs

--- Assets/02.script/VR UI/Menu/MenuSelectorMover.cs
+++ Assets/02.script/VR UI/Menu/MenuSelectorMover.cs
@@ -3,29 +3,26 @@ using VRStandardAssets.Utils;
 
 namespace VRStandardAssets.Menu
 {
-    // In the main menu, a thin bar is shown below each
-    // of the scene options.  The bar moves to below
-    // whichever option the user is currently looking at.
-    // This script moves the bar.
+
     public class MenuSelectorMover : MonoBehaviour
     {
-        [SerializeField] private float m_PopSpeed = 8f;         // How fast the bar pops in and out.
-        [SerializeField] private float m_PopDistance = 0.5f;    // How far the bar moves from it's normal position.
-        [SerializeField] private float m_MoveSpeed = 7f;        // How fast the bar moves to a new selection when the user changes what they are looking at.
-        [SerializeField] private Transform m_ParentTransform;   // The parent object in the selector hierarchy, this has no visual element.
-        [SerializeField] private Transform m_ChildTransform;    // The child object in the selector hierarchy, this has a mesh renderer to display the bar.
-        [SerializeField] private VRInteractiveItem[] m_Items;   // The VRInteractiveItems that the user can look at to move the selector.
+        [SerializeField] private float m_PopSpeed = 8f;         
+        [SerializeField] private float m_PopDistance = 0.5f;    
+        [SerializeField] private float m_MoveSpeed = 7f;        
+        [SerializeField] private Transform m_ParentTransform;   
+        [SerializeField] private Transform m_ChildTransform;    
+        [SerializeField] private VRInteractiveItem[] m_Items;   
 
 
-        private Quaternion m_TargetRotation;                    // The rotation that the selector is trying to reach.
-        private Vector3 m_StartPosition;                        // The local position of the child transform at the start.
-        private Vector3 m_PoppedPosition;                       // The local position of the child transform when it's popped out.
-        private Vector3 m_TargetPosition;                       // The local position the child transform is trying to reach.
+        private Quaternion m_TargetRotation;                    
+        private Vector3 m_StartPosition;                        
+        private Vector3 m_PoppedPosition;                       
+        private Vector3 m_TargetPosition;                       
 
 
         void Awake ()
         {
-            // Store the start position.
+            // start position.
             m_StartPosition = m_ChildTransform.localPosition;
 
             // Calculate the popped position.
@@ -35,10 +32,8 @@ namespace VRStandardAssets.Menu
 	    
         void Update ()
         {
-            // By default the target position of the child transform is unpopped.
             m_TargetPosition = m_StartPosition;
 
-            // Go through each of the interactive items and for the one that the user is looking at, set the target position and rotation.
 	        for (int i = 0; i < m_Items.Length; i++)
 	        {
 	            if (!m_Items[i].IsOver)
@@ -49,11 +44,8 @@ namespace VRStandardAssets.Menu
 	            break;
 	        }
 
-            // Set the child's local position to be closer to it's target position based on the speed it should pop in and out.
-            m_ChildTransform.localPosition = Vector3.MoveTowards (m_ChildTransform.localPosition, m_TargetPosition,
-                m_PopSpeed * Time.deltaTime);
+            m_ChildTransform.localPosition = Vector3.MoveTowards (m_ChildTransform.localPosition, m_TargetPosition, m_PopSpeed * Time.deltaTime);
 
-            // Set the parent's rotation to align with whatever VRInteractiveItem is being looked at.
             m_ParentTransform.rotation = Quaternion.Slerp(m_ParentTransform.rotation, m_TargetRotation, m_MoveSpeed * Time.deltaTime);
 	    }
     }
